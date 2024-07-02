0a4e4de4f0d13eb713183fb1af8897841ae3a0f5
gk22aan <gauravk612@gmail.com>
2024-07-03 00:15:12 +0100

    changed bow to tfidf

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import os
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # fetch the data from data/processed
 train_data = pd.read_csv('./data/processed/train_processed.csv')
@@ -19,20 +19,20 @@ y_train = train_data['sentiment'].values
 X_test = test_data['content'].values
 y_test = test_data['sentiment'].values
 
-# Apply Bag of Words (CountVectorizer)
-vectorizer = CountVectorizer(max_features=50)
+# Apply tfidfvectorizer 
+vectorizer = TfidfVectorizer(max_features=50)
 
 # Fit the vectorizer on the training data and transform it
-X_train_bow = vectorizer.fit_transform(X_train)
+X_train_tfidf = vectorizer.fit_transform(X_train)
 
 # Transform the test data using the same vectorizer
-X_test_bow = vectorizer.transform(X_test)
+X_test_tfidf = vectorizer.transform(X_test)
 
-train_df = pd.DataFrame(X_train_bow.toarray())
+train_df = pd.DataFrame(X_train_tfidf.toarray())
 
 train_df['label'] = y_train
 
-test_df = pd.DataFrame(X_test_bow.toarray())
+test_df = pd.DataFrame(X_test_tfidf.toarray())
 
 test_df['label'] = y_test
 
@@ -41,5 +41,5 @@ data_path = os.path.join("data","features")
 
 os.makedirs(data_path)
 
-train_df.to_csv(os.path.join(data_path,"train_bow.csv"))
-test_df.to_csv(os.path.join(data_path,"test_bow.csv"))
+train_df.to_csv(os.path.join(data_path,"train_tfidf.csv"))
+test_df.to_csv(os.path.join(data_path,"test_tfidf.csv"))
--- src/models/model_building.py
+++ src/models/model_building.py
@@ -5,7 +5,7 @@ import pickle
 from sklearn.ensemble import GradientBoostingClassifier
 
 # fetch the data from data/processed
-train_data = pd.read_csv('./data/features/train_bow.csv')
+train_data = pd.read_csv('./data/features/train_tfidf.csv')
 
 X_train = train_data.iloc[:,0:-1].values
 y_train = train_data.iloc[:,-1].values
